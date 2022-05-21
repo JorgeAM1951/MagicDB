bb77ad33a32ac3b2e6a2b04fcc386be0d14a6ae3
JorgeAM1951 <100132639+JorgeAM1951@users.noreply.github.com>
2022-05-21 13:29:04 +0100

    First build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,19 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebaseConfig: {
+    apiKey: 'AIzaSyC7Mae6lA6RYrCsvzkN-9XZcGex3toH0tk',
+    authDomain: 'mbd-pwm.firebaseapp.com',
+    databaseURL: 'https://mbd-pwm-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'mbd-pwm',
+    storageBucket: 'mbd-pwm.appspot.com',
+    messagingSenderId: '640999123125',
+    appId: '1:640999123125:web:f7ca4003ac825e31aa76fc',
+  },
+  production: false,
+  databaseUrl: 'https://mbd-pwm-default-rtdb.europe-west1.firebasedatabase.app'
 };
 
 /*
@@ -13,4 +23,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
